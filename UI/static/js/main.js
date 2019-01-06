5d03604863db51175a411a13f298d974c2d67549
kburudi <bkevery@gmail.com>
2019-01-06 12:16:21 +0300

    [bug #163001245]update question form js

--- UI/static/js/main.js
+++ UI/static/js/main.js
@@ -1,69 +1,59 @@
 document.addEventListener('DOMContentLoaded',
-    function () {
-        const dropFormButton = document.querySelector('button#addQuiz')
-        const dropForm = document.createElement('form')
-        const formFieldT = document.createElement('textarea')
-        const submitBtn = document.createElement('input')
+	function () {
+		const dropFormButton = document.querySelector('button#addQuiz');
+		const dropForm = document.createElement('form');
+		const formFieldT = document.createElement('textarea');
+		const submitBtn = document.createElement('input');
+		const hideButton = document.createElement('span');
 
-        dropForm.classList.add("question-form-f")
-        submitBtn.classList.add('class', "sign")
-        submitBtn.classList.add('class', "send-remark")
+		dropForm.classList.add("question-form-f");
+		submitBtn.classList.add('class', "sign");
+		submitBtn.classList.add('class', "send-remark");
+		hideButton.classList.add('fas');
+		hideButton.classList.add('fa-times');
+		hideButton.classList.add('hide-btn');
 
-        formFieldT.classList.add('textarea')
-        submitBtn.setAttribute('type', 'submit')
-        submitBtn.setAttribute('role', 'button')
-        submitBtn.setAttribute('value', 'Add question')
+		formFieldT.classList.add('textarea');
+		submitBtn.setAttribute('type', 'submit');
+		submitBtn.setAttribute('role', 'button');
+		submitBtn.setAttribute('value', 'Add question');
 
-        formFieldT.setAttribute('rows', 10)
-        formFieldT.style.borderColor = 'grey'
+		formFieldT.setAttribute('rows', 10);
 
-        dropForm.appendChild(formFieldT)
-        dropForm.appendChild(submitBtn)
+		dropForm.appendChild(formFieldT);
+		dropForm.appendChild(submitBtn);
 
-        dropFormButton.addEventListener('click', function (e) {
-            e.preventDefault()
-            this.style.display = 'none'
-            this.parentNode.setAttribute('class', 'a-question')
-            this.parentNode.appendChild(dropForm)
-        })
+		dropFormButton.addEventListener('click', function (e) {
+			e.preventDefault();
+			this.style.display = 'none';
+			this.parentNode.appendChild(dropForm);
+			this.parentNode.appendChild(hideButton);
+		})
 
-        dropForm.addEventListener('submit', function (e) {
-            e.preventDefault()
-            var value = this.querySelector('textarea').value
+		hideButton.addEventListener('click', function (e) {
+			e.preventDefault();;
+			this.parentNode.removeChild(dropForm);;
+			this.parentNode.removeChild(this);;
+			dropFormButton.style.display = "block";;
+		})
 
-            if (value == "" || value.trim() == "") {
-                e.preventDefault()
-                formFieldT.style.borderColor = "red"
-            } else {
-                contDiv = this.parentNode
-                a_question = document.createElement('p')
-                a_question.textContent = value
-                userImage = document.createElement('img')
-                userImage.setAttribute('src', '../static/images/question-main-page/questions-image.jpg')
+		dropForm.addEventListener('submit', function (e) {
+			e.preventDefault();;
+			var value = this.querySelector('textarea').value;;
 
-                a_question.classList.add('quiz-content')
-
-                contDiv.removeChild(dropForm)
-                contDiv.appendChild(userImage)
-                contDiv.appendChild(a_question)
-                dropFormButton.style.display = "block"
-            }
-        })
-
-        commentButtons = document.querySelectorAll('.comment-button')
-        commentButtons.forEach(function (btn) {
-
-            btn.addEventListener('click', function (e) {
-                e.preventDefault();
-                console.log(btn)
-                commentBox = document.createElement('div');
-                commentBox.style.display = "block";
-                commentBox.style.width = "100%";
-                submitBtn.setAttribute('value', 'Add Your comment');
-                commentBox.appendChild(dropForm);
-                btn.parentNode.appendChild(commentBox)
-                btn.style.display = "none"
-            })
-        });
-
-    })
+			if (value == "" || value.trim() == "") {
+				e.preventDefault();
+				formFieldT.style.borderColor = "red";
+			} else {
+				const questView = document.querySelector('.question-views');
+				const questBox = document.querySelector('.question-asked');
+				const newQuestion = questBox.cloneNode(true);
+				questView.appendChild(newQuestion)
+				questView.removeChild(dropFormButton.parentNode.parentNode)
+				dropFormButton.parentNode.removeChild(dropForm)
+				dropFormButton.parentNode.removeChild(hideButton)
+				dropFormButton.style.display = "block"
+				questView.appendChild(dropFormButton.parentNode.parentNode)
+			}
+		})
+	})
