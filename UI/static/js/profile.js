69f7dcf18de1c10bfe763affbf0414e40d250b2e
kburudi <bkevery@gmail.com>
2019-01-06 19:13:24 +0300

    [feature #162965635]user profile css update complete

---
+++ UI/static/js/profile.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded',
+  function () {
+    var showProf = document.querySelector('.fas.fa-bars')
+    var profile = document.querySelector('#prof-panel')
+    showProf.addEventListener('click', function (e) {
+      e.preventDefault()
+      profile.classList.remove("hide-from-prof")
+      profile.classList.add("show-on-prof")
+      this.style.display = "none"
+    })
+
+    var hideProf = document.querySelector("span.fa-times")
+    hideProf.addEventListener('click', function (e) {
+      e.preventDefault()
+      profile.classList.add("hide-from-prof")
+      profile.classList.remove("show-on-prof")
+      showProf.style.display = "block"
+    })
+
+  })
