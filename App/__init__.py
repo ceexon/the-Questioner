423f8c2ddda8f8ec4ad256c56cc9f82a79b43723
kburudi <bkevery@gmail.com>
2019-01-09 11:33:09 +0300

    [starts #163064096] update blueprint

--- App/__init__.py
+++ App/__init__.py
@@ -3,4 +3,6 @@ from App.api.v1.views.user_views import api_v1
 
 my_app = Flask(__name__)
 
+my_app.config['SECRET_KEY'] = "uudye78tgde6refs55w7iwtsj"
+
 my_app.register_blueprint(api_v1, url_prefix="/api/v1")
