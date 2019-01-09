a41e805b2a2528f3eea0e5c9d5605144cc23cd3b
kburudi <bkevery@gmail.com>
2019-01-09 10:07:00 +0300

    [starts #163064096] register blueprints

--- App/__init__.py
+++ App/__init__.py
@@ -1,1 +1,6 @@
-from flask import Flask
+from flask import Flask, Blueprint
+from App.api.v1.views.user_views import api_v1
+
+my_app = Flask(__name__)
+
+my_app.register_blueprint(api_v1, url_prefix="/api/v1")
